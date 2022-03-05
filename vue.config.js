53829915c95fe552fc50e145eeadad96077d311e
swt <396518038@qq.com>
2022-03-05 20:36:39 +0800

    修改代理路径和去除JavaBlog

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,6 @@ module.exports = {
     proxy: {
       '/api': {
         target: 'http://localhost:8888',
-        // target: 'http://39.106.49.0:8888',
         ws: true,
         changeOrigin: true,
         pathRewrite: {
